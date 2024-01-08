c72ab60320953d8b85f7506f2275254b95c7056d
sravaninellore <nellorevenkatasravani@gmail.com>
2024-01-08 14:38:27 +0000

    commit message

---
+++ src/Components/Courses/CoursePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CourseNavbar from "./CourseNavbar";
+
+function CoursePage() {
+  return (
+    <div>
+      <CourseNavbar />
+    </div>
+  );
+}
+
+export default CoursePage;
